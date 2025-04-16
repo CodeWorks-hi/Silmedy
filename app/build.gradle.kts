f803a87b9614dc32e3e6c4921e05192c19e69b9d
mybear1109 <mybear8211@gmail.com>
2025-04-16 17:51:33 +0900

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.application) // Version Catalog용
+    id("com.google.gms.google-services")    // Firebase 서비스 연동용 플러그인
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.pingtolk"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.pingtolk"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -25,6 +26,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,12 +34,36 @@ android {
 }
 
 dependencies {
-
+    // 🔹 UI 라이브러리
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // 🔹 테스트
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Firebase BoM (Bill of Materials)
+    implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
+    implementation("com.google.android.material:material:1.4.0")
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation ("com.github.bumptech.glide:glide:4.16.0") // 버전은 네 프로젝트에 맞춰서
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
+    // transform 기능 쓰려면 이거도 추가
+    implementation("jp.wasabeef:glide-transformations:4.3.0")
+
+    // Firebase Storage
+    implementation("com.google.firebase:firebase-storage:20.2.1")
+
+    // 사용 Firebase SDK (예: Analytics + Auth + Firestore 등)
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+    implementation("com.google.firebase:firebase-messaging:23.4.1")
 }
