aa990373116be9b20309598c2fab6ce7f61ef06f
codekookiz <codekookiz@gmail.com>
2025-04-24 23:10:03 +0900

    add

--- app/src/main/java/com/example/silmedy/BodyMain.java
+++ app/src/main/java/com/example/silmedy/BodyMain.java
@@ -2,12 +2,33 @@ package com.example.silmedy;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.silmedy.ui.clinic.ClinicHomeActivity;
 
 public class BodyMain extends AppCompatActivity {
+
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boby_main); // ✔️ 반드시 존재해야 함
+        setContentView(R.layout.activity_body_main); // ✔️ 반드시 존재해야 함
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("userName");
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(v -> {
+            Intent backIntent = new Intent(BodyMain.this, ClinicHomeActivity.class);
+            backIntent.putExtra("userName", username);
+            startActivity(backIntent);
+            finish();
+        });
     }
 }
