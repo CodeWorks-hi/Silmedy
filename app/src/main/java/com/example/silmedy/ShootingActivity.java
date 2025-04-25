c725ec251bcec1c381421e573e80d67442212d65
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-25 09:45:55 +0900

    yw

---
+++ app/src/main/java/com/example/silmedy/ShootingActivity.java
@@ -0,0 +1,15 @@
+package com.example.silmedy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShootingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shooting);
+
+    }
+}
