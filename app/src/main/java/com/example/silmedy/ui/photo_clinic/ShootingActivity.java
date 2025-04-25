0fa6c38b08c9ab8ee11d05a3a4d80dd480390500
codekookiz <codekookiz@gmail.com>
2025-04-25 15:29:33 +0900

    Update ShootingActivity.java

--- app/src/main/java/com/example/silmedy/ui/photo_clinic/ShootingActivity.java
+++ app/src/main/java/com/example/silmedy/ui/photo_clinic/ShootingActivity.java
@@ -10,14 +10,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
