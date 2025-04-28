c297ab6c70b0a5f26a3ac49df5e1ad7ca25f334a
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-28 11:45:56 +0900

    YW

---
+++ app/src/main/java/com/example/silmedy/ui/call/VideoCallActivity.java
@@ -0,0 +1,20 @@
+package com.example.silmedy.ui.call;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.silmedy.R;
+
+public class VideoCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_call);
+    }
+}
