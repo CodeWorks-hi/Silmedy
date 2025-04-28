fc9a04c9690714ce1d6991f0e4bcbdabc32be878
codekookiz <codekookiz@gmail.com>
2025-04-28 17:55:12 +0900

    add

--- app/src/main/java/com/example/silmedy/ui/photo_clinic/DiagnosisResultsActivity.java
+++ app/src/main/java/com/example/silmedy/ui/photo_clinic/DiagnosisResultsActivity.java
@@ -3,20 +3,20 @@ package com.example.silmedy.ui.photo_clinic;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.silmedy.R;
 import com.example.silmedy.ui.care_request.DoctorListActivity;
 
-import java.util.ArrayList;
-
 public class DiagnosisResultsActivity extends AppCompatActivity {
 
-    ImageView btnBack;
+    ImageView btnBack, imgDiagnosis;
     Button btnReservation;
+    TextView textPart, textSymptom;
 
 
     @Override
@@ -26,12 +26,24 @@ public class DiagnosisResultsActivity extends AppCompatActivity {
 
         btnBack = findViewById(R.id.btnBack);
         btnReservation = findViewById(R.id.btnReservation);
+        imgDiagnosis = findViewById(R.id.imgDiagnosis);
+        textPart = findViewById(R.id.textPart);
+        textSymptom = findViewById(R.id.textSymptom);
 
         Intent intent = getIntent();
         String username = intent.getStringExtra("user_name");
         String patient_id = intent.getStringExtra("patient_id");
         String part = intent.getStringExtra("part");
         String imagePath = intent.getStringExtra("image_path");
+
+        if (imagePath != null && !imagePath.isEmpty()) {
+            Glide.with(this)
+                .load(imagePath)
+                .into(imgDiagnosis);
+        }
+
+        textPart.setText("부위 : " + part);
+
 
 
         // AI 진단 프로세스 아래에 작성
