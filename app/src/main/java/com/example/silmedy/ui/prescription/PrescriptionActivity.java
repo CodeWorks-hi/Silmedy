dfb37283d801f8056d85e5973e47796f21c309b0
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-28 15:51:44 +0900

    yw

---
+++ app/src/main/java/com/example/silmedy/ui/prescription/PrescriptionActivity.java
@@ -0,0 +1,16 @@
+package com.example.silmedy.ui.prescription;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.silmedy.R;
+
+public class PrescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescription);
+    }
+}
