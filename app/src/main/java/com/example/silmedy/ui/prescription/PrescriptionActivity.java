f633489fab8bf07251964f34c1af8dfeccba39cb
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-29 13:02:36 +0900

    yw

--- app/src/main/java/com/example/silmedy/ui/prescription/PrescriptionActivity.java
+++ app/src/main/java/com/example/silmedy/ui/prescription/PrescriptionActivity.java
@@ -1,6 +1,12 @@
 package com.example.silmedy.ui.prescription;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +14,38 @@ import com.example.silmedy.R;
 
 public class PrescriptionActivity extends AppCompatActivity {
 
+    Intent intent = getIntent();
+    String username = intent.getStringExtra("user_name");
+    String patient_id = intent.getStringExtra("patient_id");
+
+    ImageView btnVisit, btnDelivery;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prescription);
+
+        btnVisit = findViewById(R.id.btnVisit);
+        btnDelivery = findViewById(R.id.btnDelivery);
+
+        // 약국 방문 버튼
+        btnVisit.setOnClickListener(v -> {
+            Intent visitIntent = new Intent(PrescriptionActivity.this,PharmacyListActivity.class);
+            visitIntent.putExtra("user_name",username);
+            visitIntent.putExtra("patient_id",patient_id);
+            visitIntent.putExtra("serviceType", "방문");
+            startActivity(visitIntent);
+            finish();
+        });
+
+        // 배달 신청 버튼
+        btnDelivery.setOnClickListener(v -> {
+            Intent deliveryIntent = new Intent(PrescriptionActivity.this,DeliveryInputActivity.class);
+            deliveryIntent.putExtra("user_name",username);
+            deliveryIntent.putExtra("patient_id",patient_id);
+            deliveryIntent.putExtra("serviceType", "배달");
+            startActivity(deliveryIntent);
+            finish();
+        });
     }
 }
