12ff0a8b242b34bf72f77ffc33961498ef484faa
choeyeonu2411 <choeyeonu2411@gmail.com>
2025-04-29 19:25:25 +0900

    yw

--- app/src/main/java/com/example/silmedy/ui/user/MyPageActivity.java
+++ app/src/main/java/com/example/silmedy/ui/user/MyPageActivity.java
@@ -1,17 +1,44 @@
 package com.example.silmedy.ui.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.silmedy.R;
+import com.example.silmedy.ui.auth.FindPasswordActivity;
 
 public class MyPageActivity extends AppCompatActivity {
+
+    Intent intent = getIntent();
+    String username = intent.getStringExtra("user_name");
+
+    Button btnChangeProfile,btnChangePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
+
+        btnChangeProfile = findViewById(R.id.btnChangeProfile);
+        btnChangePassword = findViewById(R.id.btnChangePassword);
+
+        // 내 정보 수정 하기
+        btnChangeProfile.setOnClickListener(v -> {
+            Intent changProfileIntent = new Intent(MyPageActivity.this,MyEditActivity.class);
+            changProfileIntent.putExtra("user_name",username);
+            startActivity(changProfileIntent);
+            finish();
+        });
+
+        // 비밀 번호 변경
+        btnChangePassword.setOnClickListener(v -> {
+            Intent changPasswordIntent = new Intent(MyPageActivity.this, FindPasswordActivity.class);
+            changPasswordIntent.putExtra("user_name",username);
+            startActivity(changPasswordIntent);
+            finish();
+        });
 
     }
 }
